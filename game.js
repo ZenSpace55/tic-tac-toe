17108f1fd564067dba91fa75ba4b4b76a6302d45
Zero1 <mmanickii@gmail.com>
2021-12-12 19:23:17 -0500

    Implemented the basic  game logic, X and O take turns, game checks for victory or a draw after each turn

--- game.js
+++ game.js
@@ -1,41 +1,92 @@
+const Game = (() => {
+
+    let turn = "X";
+
+    const endTurn = function(){
+
+        console.log("attempting turn switch");
+        let gameEnded = Gameboard.checkForWin();
+        if (gameEnded === "X"){
+            console.log("x victory");
+        }
+        else if (gameEnded === "O"){
+            console.log("o victory");
+        }
+        else if (gameEnded === "D"){
+            console.log("board full, DRAW");
+        }
+        else console.log("game continues" + gameEnded);
+        if (this.turn === "X"){
+            this.turn = "O";
+        }
+        else{
+            this.turn = "X";
+        }
+    }
+
+    return {turn, endTurn};
+})();
+
 const Gameboard = (() => {
     //Game board consisting of 9 open spaces
     const boardSlots = [];
     //An array to track which board slots are still available to play
     const openSlots = [];
+
+    const playSquare = function(square){
+        boardSlots[square].textContent = Game.turn;
+        const index = openSlots.indexOf(square);
+        openSlots.splice(index, 1);
+        console.log(openSlots.length + " spaces remaining");
+        Game.endTurn();
+    }
+
+    const prepareBoard = function(){
+        for (let i = 0; i < 9; i++){
+            boardSlots.push(document.getElementById(i));
+            openSlots.push(i);
+            boardSlots[i].addEventListener('click', () =>{
+                playSquare(i);
+            });
+        }
+        console.log("Open spaces" + openSlots.length);
+        console.log("Number of squares: " + boardSlots.length);
+        console.log("slot three: " + boardSlots[2].textContent);
+    }
+
     const checkForWin = function(){
         let boardFull = true;
         for (let i = 0; i < 3; i++){
             //scans the board vertically and horizontally, looking for 3 in a row
-            let boardStatusX = boardSlots[i*3];
-            boardStatusX += boardSlots[(i*3)+1];
-            boardStatusX += boardSlots[(i*3)+2];
-            let boardStatusY = boardSlots[i%3];
-            boardStatusY += broadSlots[(i%3)+3];
-            boardStatusY += boardSlots[(i%3)+6];
+            let boardStatusX = boardSlots[i*3].textContent;
+            boardStatusX += boardSlots[(i*3)+1].textContent;
+            boardStatusX += boardSlots[(i*3)+2].textContent;
+            let boardStatusY = boardSlots[i%3].textContent;
+            boardStatusY += boardSlots[(i%3)+3].textContent;
+            boardStatusY += boardSlots[(i%3)+6].textContent;
             if (boardStatusX === "XXX" || boardStatusY === "XXX"){
-                //X win
+                return "X";
             }
             else if (boardStatusX === "OOO" || boardStatusY === "OOO"){
-                ///O win
+                return "O"
             }
         }
         //bruteforce check (since there are just two cases) if there is a diagonal win
-        let boardDiagonal1 = boardSlots[2] + boardSlots[4] + boardSlots[6];
-        let boardDiagonal2 = boardSlots[0] + boardSlots[4] + boardSlots[8];
+        let boardDiagonal1 = boardSlots[2].textContent + boardSlots[4].textContent + boardSlots[6].textContent;
+        let boardDiagonal2 = boardSlots[0].textContent + boardSlots[4].textContent + boardSlots[8].textContent;
         if (boardDiagonal1 === "XXX" || boardDiagonal2 === "XXX"){
-            //X win
+            return "X";
         }
         else if (boardDiagonal1 === "OOO" || boardDiagonal2 === "OOO"){
-            ///O win
+            return "O";
         }
-        if (openSlots.length === 0){
-            //no open slots left, no winner, game = draw
+        else if (openSlots.length === 0){
+            return "D"; //for 'draw'
         }
     }
 
     return {
-        boardSlots, checkForWin
+        boardSlots, prepareBoard, checkForWin
     };
 })();
 
@@ -43,6 +94,16 @@ const Player = (name) => {
     return {name};
 }
 
+function renderBoard(){
+
+}
+
 function InitializeGame(){
 
 }
+
+function playSquare(square){
+    Gameboard.playSquare(square);
+}
+
+Gameboard.prepareBoard();
